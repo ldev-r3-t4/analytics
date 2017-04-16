dd383613ac919eaf9ee3eb1a14a97f2d09f5e6a4
Geordon Worley <vadixidav@gmail.com>
2017-04-15 20:15:37 -0400

    add index HTML page

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,31 @@ extern crate analytics_proto as proto;
 pub mod v1;
 
 use rocket::Rocket;
+use rocket::response::content::HTML;
 
 fn countdown() -> Rocket {
     rocket::ignite()
+        .mount("/", routes![index])
         .mount("/v1", v1::routes())
 }
 
 fn main() {
     countdown().launch();
+}
+
+#[get("/")]
+pub fn index() -> HTML<&'static str> {
+    HTML(r#"
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <title>Analytics Tracking</title>
+    </head>
+    <body>
+
+    <p>See the documentation <a href="https://ldev-r3-t4.github.io/analytics/analytics/">here</a>.</p>
+
+    </body>
+    </html>
+    "#)
 }
